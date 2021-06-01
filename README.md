a1feca4bcbe0a663b9772c01cbadba0ab8387925
P1M5 <playeronemilionandfive@gmail.com>
2021-06-01 21:52:09 +0300

    Fixed invite links

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Please edit the [config file](https://github.com/P1M5/discord-testBotty/blob/main/config.json) before trying to run the bot
 
+## Invite
+
+You should also consider editing the [invite command's](https://github.com/P1M5/discord-testBotty/blob/main/commands/general_commands/invite.js) links
--- commands/general_commands/invite.js
+++ commands/general_commands/invite.js
@@ -10,7 +10,7 @@ module.exports = {
             .setColor("DARK_BLUE")
             .setDescription("Join the support server to ask questions, report bugs and meet an awesome community or use the bot invite link to invite the bot to your server!")
             .addFields(
-                {name: "Support Server Invite", value: "[Join the server](https://discord.gg/matPPRBM)"},
+                {name: "Support Server Invite", value: "[Join the server](https://discord.gg/VbKNuUd49M)"},
         {name: "Bot Invite", value: "[Invite the bot](https://discord.com/api/oauth2/authorize?client_id=453517745611341826&permissions=470674551&scope=bot)"},
             )
             .setTimestamp()
