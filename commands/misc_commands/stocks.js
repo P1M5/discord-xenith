d595633ae0d60828d25c440754c8c1a445226216
P1M5 <playeronemilionandfive@gmail.com>
2021-06-04 14:48:37 +0300

    Fix crypto

---
+++ commands/misc_commands/stocks.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "stocks",
+    description: "Display information about stocks and cryptocurrency",
+    aliases: ["cryptocurrency","crypto","stocks","$","investing","currency","stonk"],
+    category: "Miscellaneous",
+    usage: "<stock name,currency name,cryptocurrency name or id>",
+    execute (message, args) {
+
+    },
+  }
