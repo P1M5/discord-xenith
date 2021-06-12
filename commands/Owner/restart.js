dc624c5f669c78a195070cd90aebf39ff233d0a9
P1M5 <playeronemilionandfive@gmail.com>
2021-06-12 19:10:10 +0300

    Atom left some git code in restart command

--- commands/Owner/restart.js
+++ commands/Owner/restart.js
@@ -4,19 +4,11 @@ module.exports = class Restart extends BasicCommand {
     static name = "restart";
     static description = "Restarts the bot";
     static ownerOnly = true;
-<<<<<<< HEAD:commands/Owner/restart.js
     static execute(msgToken) {
 
     	msgToken.message.react("👌").then(() =>
     		process.exit())
     		.catch(e =>
     			console.error(e));
-=======
-    static execute (msgToken) {
-        msgToken.message.react("👌").then(msg =>
-        process.exit())
-        .catch(e =>
-        console.error(e));
->>>>>>> e89ad83643580747de6feb9eee714f07f67ab787:commands/owner/restart.js
     }
 };
