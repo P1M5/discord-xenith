e45d7c27fd52c93839de58d13336deceaf101da7
P1M5 <playeronemilionandfive@gmail.com>
2021-06-21 16:46:37 +0300

    Fixed unban description, fixed urban not working properly

--- commands/Informative/urban.js
+++ commands/Informative/urban.js
@@ -1,5 +1,5 @@
 const ud = require("urban-dictionary");
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, Util } = require("discord.js");
 const { BasicCommand } = require("../../abstractClasses/BasicCommand.js");
 
 
@@ -22,72 +22,67 @@ class Urban extends BasicCommand {
              static execute(msgToken) {
 
              	const args = msgToken.args.split(/ +/);
-
-             	let num = 0;
-             	let sliceEnd = args.length;
-
-             	if(parseInt(args[args.length - 1]) && args.length > 1) num = parseInt(args[args.length - 1]);
-             	if(num < 0 || num > 9) return msgToken.message.channel.send("Definition number needs to be between `0` and `9`");
-             	if(num !== 0) sliceEnd = sliceEnd - 1;
+             	let sliceEnd = args.length - 1;
+             	let numb = parseInt(args.slice(args.length - 1, args.length));
+             	if (isNaN(numb)) {
+             		numb = 0;
+             		sliceEnd = args.length;
+             	}
+             	const uinput = args.slice(0, sliceEnd).join(" ");
 
              	class udEmbed {
 
-             		constructor(res) {
+             		constructor(res, number = 0, wotd = false) {
 
+             			let titleDef = `(Definition #${number})`;
+             			if(wotd) titleDef = `(Wotd #${number})`;
+             			if(number === 0) titleDef = "";
              			const Defin = {};
-             			Object.entries(res[0]).forEach(([key, prop]) => {
+             			Object.entries(res[number]).forEach(([key, prop]) => {
              				Defin[key] = prop;
              			});
 
              			 const embedSend = new MessageEmbed()
-             			.setTitle(Defin.word)
+             			.setTitle(`${Defin.word} ${titleDef}`)
              			.setColor("DARK_PURPLE")
              			.setFooter(`Author: ${Defin.author} | Written: ${new Date(Defin.written_on).toLocaleString("en-GB")} | ${msgToken.message.client.ws.ping} ms`)
              			.setTimestamp()
              			.setDescription(Defin.definition)
-             			.addField("Example:", Defin.example);
-
              			msgToken.message.channel.send(embedSend);
              		}
              	}
+             	if(args[0] != "" && args[0] != "wotd") {
+             		ud.define(uinput).then((results) => {
 
-             	if(args.length == 0) {
+             			new udEmbed(results, numb);
+             			console.log(args[0]);
+             		})
+             			.catch((e) => {
+             				console.log(`Urban Dictionary: ${e}`);
+             				if(e.message == "Cannot convert undefined or null to object") msgToken.message.channel.send("Out of range, try a lower number");
+             			});
+             	}
+             	else if (args[0] == "wotd") {
+             		ud.wordsOfTheDay().then((results) => {
+
+             			new udEmbed(results, numb, true);
+             		})
+             			.catch((e) => {
+             				console.log(`Urban Dictionary: ${e}`);
+             			});
+             	}
+             	else {
              		ud.random().then((results) => {
 
              			new udEmbed(results);
-
-             		}).catch((error) => {
-
-             			console.error(`Urban dictionary error: ${error.message}`);
-             			msgToken.message.channel.send(error.message);
-
-             		});
-             	}
-             	else if (args[0] === "wotd") {
-             		ud.wordsOfTheDay().then((results) => {
-
-             			new udEmbed(results);
-
-             		}).catch((error) => {
-
-             			console.error(`Urban dictionary error: ${error.message}`);
-             			if(error.message === "Cannot convert undefined or null to object") return msgToken.message.channel.send("Definition number not found, try a lower number");
-             			msgToken.message.channel.send(error.message);
-
-             		});
-             	}
-             	else {
-             		ud.define(args.slice(0, sliceEnd).toString()).then((results) => {
-
-             			new udEmbed(results);
-
-             		}).catch((error) => {
-             			console.error(`Urban dictionary error: ${error.message}`);
-             			if(error.message === "Cannot convert undefined or null to object") return msgToken.message.channel.send("Definition number not found, try a lower number");
-             			msgToken.message.channel.send(error.message);
-             		});
+             		})
+             			.catch((e) => {
+             				console.log(`Urban Dictionary: ${e}`);
+             			});
              	}
              }
+
+
 }
 
 module.exports = Urban;
